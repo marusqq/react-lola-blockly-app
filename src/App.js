ba42934779fd569bb829716dbb891c929d63f5a1
Marius Pozniakovas <pozniakovui@gmail.com>
2023-01-08 21:21:10 +0200

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 
+import Blockly from "blockly";
+import { createPlayground, toolboxCategories } from "@blockly/dev-tools";
+
+
+// goog.require('Blockly.minimalist.Renderer');
+// goog.require('Blockly.Themes.Zelos');///
+
 function App() {
+  // start()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <div id="blocklyArea"></div> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,10 +25,31 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
 }
+
+const createWorkspace = (blocklyDiv, options) => {
+  return Blockly.inject(blocklyDiv, options);
+};
+
+const defaultOptions = {
+  toolbox: toolboxCategories
+};
+
+createPlayground(
+  document.getElementById("blocklyArea"),
+  createWorkspace,
+  defaultOptions,
+  /** PlaygroundConfig */ {},
+  /** Monaco Editor Path */ "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.19.2/min/vs"
+);
+
+function start() {
+  console.log('test')
+}
+
 
 export default App;
