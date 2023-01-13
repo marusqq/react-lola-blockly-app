516afc6dd7d45cd89c1251cce22081a088360b04
Marius Pozniakovas <pozniakovui@gmail.com>
2023-01-13 23:56:57 +0200

    updating lola compiling

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const fs = require('fs')
 const subProcess = require('child_process')
+var bodyParser = require('body-parser')
 
 const app = express(); //Line 2
 const port = process.env.PORT || 5000;
 
-function getTime(){
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded());     // to support URL-encoded bodies
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+
+function getTime() {
     let date = new Date();
-    let currentDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
-    let currentTime = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-    let datetime = currentDate+" "+currentTime
+    let currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    let currentTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    let datetime = currentDate + " " + currentTime
     return datetime
 }
 
@@ -23,44 +30,25 @@ app.post('/compile_lola', (req, res) => {
 
     let resp = {
         'status': 200,
-        'compiled': false,
+        'compiled': true,
         'verilogCode': ''
     }
 
     // set unique name for files: blockly_yy_mm_dd_HH_MM_SS.Lola
     let date = new Date();
-    let currentDate = date.getFullYear()+"_"+(date.getMonth()+1)+"_"+ date.getDate();
-    let currentTime = date.getHours()+"_"+date.getMinutes()+"_"+ date.getSeconds();
+    let currentDate = date.getFullYear() + "_" + (date.getMonth() + 1) + "_" + date.getDate();
+    let currentTime = date.getHours() + "_" + date.getMinutes() + "_" + date.getSeconds();
 
-    let datetime = currentDate+"_"+currentTime
+    let datetime = currentDate + "_" + currentTime
 
     let filenameLola = `compile/blockly_${datetime}.Lola`
     let filenameVerilog = `compile/blockly_${datetime}.v`
 
     // get lola code from blockly
-    let lolaCode =
-`MODULE Multiplier (     (*NW 13.9.2014*)
-  IN clk, run, u: BIT;
-  OUT stall: BIT;
-  IN x, y: WORD;   (*32 bit*)
-  OUT z: [64] BIT);
-
-  REG (clk) S: [6] BIT;   (*state*)
-    P: [64] BIT;   (*produfdsfdsct*)
-  VAR w0: WORD;
-    w1: [33] BIT;
-
-BEGIN stall := run & (S # 33);
-  w0 := P.0 -> y : 0;
-  w1 := (S =32) & u -> {P.63, P[63:32]} - {w0.31, w0} : {P.63, P[63:32]} + {w0.31, w0};
-  S := run -> S+1 : 0;
-  P := (S = 0) -> {0'32, x} : {w1[32:0], P[31:1]};
-  z := P;
-END Multiplier.
-`
+    let lolaCode = req.body.code
 
     // call received
-    console.log(`${getTime()} Compile Lola POST call received`)
+    console.log(`${getTime()} Compile Lola POST call received with body:\n ${JSON.stringify(req.body)}`)
 
     // create a file with that lola code ,
     console.log(`${getTime()} Creating file: ${filenameLola}`)
@@ -68,22 +56,28 @@ END Multiplier.
 
     // compile that file with Lola executable
     console.log(`${getTime()} Compiling .Lola file`)
-    subProcess.exec(`../Lola ${filenameLola} ${filenameVerilog}`, (err, stdout, stderr) => {
+    subProcess.exec(`../Lola ${filenameLola} ${filenameVerilog}`,
+        (err, stdout, stderr) => {
         if (err) {
             console.error(`${getTime()}: error: ${err}`)
             process.exit(1)
         } else {
             let out = stdout.toString()
-            // let err = stderr.toString()
 
             // check output of compilation
-            let lines_of_output = out.split('\n')
-            if (lines_of_output.length > 4) {
-                resp.compiled = lines_of_output[4].includes('done')
+            let linesOfOutput = out.split('\n')
 
-                // if compiled, get verilog file info
-                resp.verilogCode = fs.readFileSync(filenameVerilog).toString()
+            for (const lineOfOutput of linesOfOutput) {
+                if (lineOfOutput.includes('compilation failed')) {
+                    resp.compiled = false
+                    break
+                }
             }
+            
+            // if compiled, get verilog file info
+            if (resp.compiled)
+                resp.verilogCode = fs.readFileSync(filenameVerilog).toString()
+
 
             // delete files
             for (const filename of [filenameLola, filenameVerilog]) {
--- src/Lola/compiler.js
+++ src/Lola/compiler.js
@@ -1,33 +1,25 @@
 import * as lola from './lola.js';
 import Blockly from 'blockly';
 
+function save(filename, data) {
+    const blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
+}
+
+
 export default function compileLola() {
 
     let generatedCode = lola.generator.workspaceToCode(Blockly.getMainWorkspace())
-    console.log(generatedCode)
-    console.log(typeof(generatedCode))
-    // let requestData = {'code': generatedCode}
-    // console.log('will send requestData')
-
-    // fetch('/compile_lola')
-    //     .then(response => {
-    //         return response.json();
-    //     })
-    //     .then(lolaCompiled => {
-    //         console.log(lolaCompiled)
-    //     })
-
-    // fetch('/compile_lola', {
-    //     method: "POST",
-    //     headers: {
-    //         Accept: "application/json",
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //         username: "foo",
-    //         password: "bar"
-    //     })
-    // }).then()
 
     fetch('/compile_lola', {
         method: 'POST',
@@ -35,18 +27,27 @@ export default function compileLola() {
             Accept: "application/json",
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({'code': generatedCode}),
+        body: JSON.stringify( {'code': generatedCode})
     })
         .then((response) => response.json())
         .then((data) => {
             console.log('Success:', data);
-            alert('pasol nx desriau tu')
+            if (data.compiled) {
+                let download = window.confirm('' +
+                    'Lola code was compiled and is correct\n' +
+                    'Do you want to download it as verilog?')
+
+                if (download)
+                    save('verilog.v', data.verilogCode)
+
+            }
+            else
+                alert('Lola code is incorrect')
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 
-    // console.log(a)
 }
 
 
